1bb837cebc94a68611d131588d837a3d3b9edcba
roshanbandara <47655552+roshanbandara@users.noreply.github.com>
2019-06-15 06:28:15 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,20 +15,23 @@
 
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+/*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "10px 10px";
     document.getElementById("logo").style.fontSize = "20px";
+
     // document.getElementById("logo").style.display = "none";
+
   } else {
     document.getElementById("navbar").style.padding = "15px 10px";
     document.getElementById("logo").style.fontSize = "35px";
+
     // document.getElementById("logo").style.display = "block";
     // document.getElementById("navbar").style.transition = "transform 0.4s ease";
   }
-}
+}*/
 
 
 
